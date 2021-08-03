92fe14b0f7536a6dd613c8accb0a3c139612eb15
Richie Wee Salim <ricia1212@gmail.com>
2021-08-03 16:28:14 +0800

    Simple Calculator Finished

--- src/views/finance-calculator.js
+++ src/views/finance-calculator.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { evaluate } from 'mathjs'
+import DisplayScreen from '../components/DisplayScreen'
+import ShowButtons from '../components/ShowButtons'
+import ErrorComponent from '../components/ErrorComponent'
+import Title from '../components/Title'
 
 const FinanceCalculator = () =>{
 
--- src/views/simple-calculator.js
+++ src/views/simple-calculator.js
@@ -1,12 +1,63 @@
-import React from 'react'
+import { useState } from 'react';
+import { evaluate } from 'mathjs'
+import DisplayScreen from '../components/DisplayScreen'
+import ShowButtons from '../components/ShowButtons'
+import ErrorComponent from '../components/ErrorComponent'
+import Title from '../components/Title'
+import '../styles/simple.scss'
 
-const SimpleCalculator = () =>{
+const SimpleCalculator = () => {
+  const calcExpression = {
+    displayExpression: '',
+    displayValue: 0,
+    showError: false
+  }
+  
+  const [calcExp, setCalcExp] = useState(calcExpression)
 
+  const computeValue = () => {
+    setCalcExp({ ...calcExp, displayValue: evaluate(calcExp.displayExpression), displayExpression: '' })
+  }
 
-    
-    return(
-        <div></div>
-    );
+  const computeDisplayExpression = (e) => {
+    if (e.target.id == 'CL') {
+      setCalcExp({ ...calcExp, displayExpression: '', displayValue: 0 })
+
+    } else if (calcExp.displayExpression == '' && e.target.id.match(/[0-9]/g) == null) {
+
+      setCalcExp({ ...calcExp, displayExpression: '', showError: true })
+
+      setTimeout(() => {
+        setCalcExp({ ...calcExp, showError: false })
+      }, 3000)
+
+    } else {
+
+      setCalcExp({
+        ...calcExp,
+        displayExpression: calcExp.displayExpression + e.target.id
+      })
+    }
+  }
+
+  const { displayValue, displayExpression, showError } = calcExp
+
+  return (
+    <div className='Container'>
+      <Title />
+      <div className="buttonDisplay">
+				{showError && <ErrorComponent msg={'Please enter a number first'} />}
+
+				<DisplayScreen
+					displayExpression={displayExpression}
+					displayValue={displayValue} />
+
+				<ShowButtons
+					computeValue={computeValue}
+					computeDisplayExpression={computeDisplayExpression} />
+      </div>
+    </div>
+  );
 }
 
 export default SimpleCalculator;
